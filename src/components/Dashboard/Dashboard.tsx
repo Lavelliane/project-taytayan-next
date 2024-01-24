01b98c904cca1de5fb2f402f3b9b3e4c8a552a73
Johnfil Initan <njohnfil.pr+github@gmail.com>
2024-01-25 01:49:45 +0800

    Added name initials as placeholder for avatars

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -10,11 +10,18 @@ const Dashboard = () => {
     useEffect(() => {
       console.log(user)
     }, [user])
+
+
+
+    let firstNameOnly = ''
+    if (user && user.firstName) {
+      firstNameOnly= user.firstName.split(' ')[0];
+    }
     
     return (
       <main className='flex flex-col w-full px-4 md:px-8 lg:px-12 xl:px-24'>
         <section className='py-8'>
-          <h1 className='font-inter font-semibold pb-8 text-2xl text-dark'>Welcome back, Jhury.</h1>
+          <h1 className='font-inter font-semibold pb-8 text-2xl text-dark'>Welcome back, {firstNameOnly}.</h1>
           <DashboardMetrics activeTrainings={232} jobsAvailable={132} upcomingEvents={13} />
         </section>
         <section>
