d40472135406007734ffb82e0890460fa3ed790c
Vince Abella <vinceabella05@gmail.com>
2024-01-29 03:03:08 +0800

    merge conflict and resolve issues

--- src/components/Networking/AllEventsPage.tsx
+++ src/components/Networking/AllEventsPage.tsx
@@ -1,146 +1,128 @@
-import React, { useState, useEffect } from "react";
-import { CategoryBadge } from "../Trainings/CategoryBadge";
-import { CategoryDropdown } from "../Trainings/CategoryDropdown";
-import { NetworkingEvent } from "@/types/types";
+import React, { useState, useEffect } from 'react';
+import { CategoryBadge } from '../Trainings/CategoryBadge';
+import { CategoryDropdown } from '../Trainings/CategoryDropdown';
+import { NetworkingEvent } from '@/types/types';
 
-import { SortDropdown } from "@/components/Trainings/SortDropdown";
-import { AddEventButton } from "./AddEventButton";
-import { collection, getDocs, where, query } from "firebase/firestore";
-import { useAuthStore } from "@/hooks/useAuth";
-import { db } from "@/lib/firebase";
-import { NetworkingEventCard } from "./NetworkingEventCard";
+import { SortDropdown } from '@/components/Trainings/SortDropdown';
+import { AddEventButton } from './AddEventButton';
+import { collection, getDocs, where, query } from 'firebase/firestore';
+import { useAuthStore } from '@/hooks/useAuth';
+import { db } from '@/lib/firebase';
+import { NetworkingEventCard } from './NetworkingEventCard';
 
 const AllEventsPage = () => {
-  const [networkingEvents, setNetworkingEvents] = useState<NetworkingEvent[]>(
-    []
-  );
-  const [sortOption, setSortOption] = useState<string>("alphabetical"); // Default sorting option
-  const [selectedCategories, setSelectedCategories] = useState<string[]>([]); // Initialize
-  const [filteredEvents, setFilteredEvents] = useState<any[]>(networkingEvents);
+	const [networkingEvents, setNetworkingEvents] = useState<NetworkingEvent[]>([]);
+	const [sortOption, setSortOption] = useState<string>('alphabetical'); // Default sorting option
+	const [selectedCategories, setSelectedCategories] = useState<string[]>([]); // Initialize
+	const [filteredEvents, setFilteredEvents] = useState<any[]>(networkingEvents);
 
-  useEffect(() => {
-    fetchEvents();
-  }, []);
+	useEffect(() => {
+		fetchEvents();
+	}, []);
 
-  useEffect(() => {
-    setFilteredEvents(networkingEvents);
-  }, [networkingEvents]);
+	useEffect(() => {
+		setFilteredEvents(networkingEvents);
+	}, [networkingEvents]);
 
-  useEffect(() => {
-    sortEvents(sortOption);
-  }, [sortOption]);
+	useEffect(() => {
+		sortEvents(sortOption);
+	}, [sortOption]);
 
-  useEffect(() => {
-    filterEvents();
-  }, [selectedCategories]);
+	useEffect(() => {
+		filterEvents();
+	}, [selectedCategories]);
 
-  const fetchEvents = async () => {
-    try {
-      const eventsRef = query(collection(db, "networking"));
-      const eventsDoc = await getDocs(eventsRef);
-      const fetchedEvents: NetworkingEvent[] = [];
-      eventsDoc.forEach((doc) => {
-        const eventData: NetworkingEvent = {
-          ...(doc.data() as NetworkingEvent),
-        };
-        console.log(eventData);
-        fetchedEvents.push(eventData);
-      });
-      const recentEvents = fetchedEvents.slice(0, 4);
-      setNetworkingEvents(recentEvents);
-    } catch (e) {
-      console.error(e);
-    }
-  };
+	const fetchEvents = async () => {
+		try {
+			const eventsRef = query(collection(db, 'networking'));
+			const eventsDoc = await getDocs(eventsRef);
+			const fetchedEvents: NetworkingEvent[] = [];
+			eventsDoc.forEach((doc) => {
+				const eventData: NetworkingEvent = {
+					...(doc.data() as NetworkingEvent),
+				};
+				console.log(eventData);
+				fetchedEvents.push(eventData);
+			});
+			const recentEvents = fetchedEvents.slice(0, 4);
+			setNetworkingEvents(recentEvents);
+		} catch (e) {
+			console.error(e);
+		}
+	};
 
-  const handleSortChange = (newSortOption: string) => {
-    setSortOption(newSortOption);
-    sortEvents(newSortOption);
-  };
+	const handleSortChange = (newSortOption: string) => {
+		setSortOption(newSortOption);
+		sortEvents(newSortOption);
+	};
 
-  const sortEvents = (option: string) => {
-    let sortedEvents: any[] = [...filteredEvents];
+	const sortEvents = (option: string) => {
+		let sortedEvents: any[] = [...filteredEvents];
 
-    switch (option) {
-      case "alphabetical":
-        sortedEvents.sort((a, b) =>
-          a.trainingName.localeCompare(b.trainingName)
-        );
-        break;
-      case "date":
-        sortedEvents.sort((a, b) => b.trainingDate - a.trainingDate);
-        break;
-      case "registrants":
-        sortedEvents.sort(
-          (a, b) =>
-            b.trainingRegistrants?.length - a.trainingRegistrants?.length
-        );
-        break;
-      default:
-        break;
-    }
+		switch (option) {
+			case 'alphabetical':
+				sortedEvents.sort((a, b) => a.trainingName.localeCompare(b.trainingName));
+				break;
+			case 'date':
+				sortedEvents.sort((a, b) => b.trainingDate - a.trainingDate);
+				break;
+			case 'registrants':
+				sortedEvents.sort((a, b) => b.trainingRegistrants?.length - a.trainingRegistrants?.length);
+				break;
+			default:
+				break;
+		}
 
-    setFilteredEvents(sortedEvents);
-  };
+		setFilteredEvents(sortedEvents);
+	};
 
-  const handleCategoryChange = (newSelectedCategories: string[]) => {
-    setSelectedCategories(newSelectedCategories);
-  };
+	const handleCategoryChange = (newSelectedCategories: string[]) => {
+		setSelectedCategories(newSelectedCategories);
+	};
 
-  const filterEvents = () => {
-    if (selectedCategories.length > 0) {
-      setFilteredEvents(
-        networkingEvents.filter((event) =>
-          selectedCategories.includes(event.eventCategory)
-        )
-      );
-    } else {
-      setFilteredEvents(networkingEvents);
-    }
-  };
+	const filterEvents = () => {
+		if (selectedCategories.length > 0) {
+			setFilteredEvents(networkingEvents.filter((event) => selectedCategories.includes(event.eventCategory)));
+		} else {
+			setFilteredEvents(networkingEvents);
+		}
+	};
 
-  return (
-    <main className="flex flex-col w-full p-4 md:p-6 lg:p-8 xl:p-10">
-      <section className="pb-8">
-        <h1 className="font-inter font-semibold pb-4 text-lg text-dark">
-          All Events
-        </h1>
-        <div>
-          <div className="pb-8">
-            <div className="flex gap-6 justify-between">
-              <div className="flex gap-6">
-                <CategoryDropdown
-                  selectedCategories={selectedCategories}
-                  onCategoryChange={handleCategoryChange}
-                />
-                <SortDropdown onSortChange={handleSortChange} />
-              </div>
-              <AddEventButton />
-            </div>
-            <div className="flex flex-wrap gap-2 pt-4 ">
-              {selectedCategories
-                .slice()
-                .sort((a, b) => a.localeCompare(b))
-                .map((category, index) => (
-                  <CategoryBadge key={index} category={category} />
-                ))}
-            </div>
-          </div>
-          <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 w-full pb-8 bg-slate-50 p-6 rounded-xl'>
-            {filteredEvents.map((event) => (
-              <NetworkingEventCard
-                key={event.eventId}
-                networkingEventData={event}
-              />
-            ))}
-            {networkingEvents.length === 0 || filteredEvents.length === 0 && (
-              <h1 className="justify-center font-semibold text-center col-span-full py-24">No events found</h1>
-            )}
-          </div>
-        </div>
-      </section>
-    </main>
-  );
+	return (
+		<main className='flex flex-col w-full p-4 md:p-6 lg:p-8 xl:p-10'>
+			<section className='pb-8'>
+				<h1 className='font-inter font-semibold pb-4 text-lg text-dark'>All Events</h1>
+				<div>
+					<div className='pb-8'>
+						<div className='flex gap-6 justify-between'>
+							<div className='flex gap-6'>
+								<CategoryDropdown selectedCategories={selectedCategories} onCategoryChange={handleCategoryChange} />
+								<SortDropdown onSortChange={handleSortChange} />
+							</div>
+							<AddEventButton />
+						</div>
+						<div className='flex flex-wrap gap-2 pt-4 '>
+							{selectedCategories
+								.slice()
+								.sort((a, b) => a.localeCompare(b))
+								.map((category, index) => (
+									<CategoryBadge key={index} category={category} style={''} />
+								))}
+						</div>
+					</div>
+					<div className='grid grid-cols-1 lg:grid-cols-2 gap-6 w-full pb-8 bg-slate-50 p-6 rounded-xl'>
+						{filteredEvents.map((event) => (
+							<NetworkingEventCard key={event.eventId} networkingEventData={event} />
+						))}
+						{networkingEvents.length === 0 ||
+							(filteredEvents.length === 0 && (
+								<h1 className='justify-center font-semibold text-center col-span-full py-24'>No events found</h1>
+							))}
+					</div>
+				</div>
+			</section>
+		</main>
+	);
 };
 
 export default AllEventsPage;
--- src/components/Trainings/AllTrainingsPage.tsx
+++ src/components/Trainings/AllTrainingsPage.tsx
@@ -1,146 +1,127 @@
-import React, { useState, useEffect } from "react";
-import { CategoryBadge } from "../Trainings/CategoryBadge";
-import { CategoryDropdown } from "../Trainings/CategoryDropdown";
-import { TrainingCard } from "../Trainings/TrainingCard";
-import { Training } from "@/types/types";
-import { DefaultTraining } from "@/utils/DefaultProfile";
-import { SortDropdown } from "./SortDropdown";
-import { AddTrainingButton } from "./AddTrainingButton";
-import { collection, getDocs, where, query } from "firebase/firestore";
-import { useAuthStore } from "@/hooks/useAuth";
-import { db } from "@/lib/firebase";
+import React, { useState, useEffect } from 'react';
+import { CategoryBadge } from '../Trainings/CategoryBadge';
+import { CategoryDropdown } from '../Trainings/CategoryDropdown';
+import { TrainingCard } from '../Trainings/TrainingCard';
+import { Training } from '@/types/types';
+import { DefaultTraining } from '@/utils/DefaultProfile';
+import { SortDropdown } from './SortDropdown';
+import { AddTrainingButton } from './AddTrainingButton';
+import { collection, getDocs, where, query } from 'firebase/firestore';
+import { useAuthStore } from '@/hooks/useAuth';
+import { db } from '@/lib/firebase';
 
 const AllTrainingsPage = () => {
-  const userStore = useAuthStore((state) => state.user);
-  const [trainings, setTrainings] = useState<Training[]>([]);
+	const userStore = useAuthStore((state) => state.user);
+	const [trainings, setTrainings] = useState<Training[]>([]);
 
-  useEffect(() => {
-    fetchTrainings();
-  }, []);
+	useEffect(() => {
+		fetchTrainings();
+	}, []);
 
-  useEffect(() => {
-    setFilteredTrainings(trainings);
-  }, [trainings]);
+	useEffect(() => {
+		setFilteredTrainings(trainings);
+	}, [trainings]);
 
-  const fetchTrainings = async () => {
-    try {
-      const trainingRef = collection(db, "trainings");
-      const trainingDoc = await getDocs(trainingRef);
+	const fetchTrainings = async () => {
+		try {
+			const trainingRef = collection(db, 'trainings');
+			const trainingDoc = await getDocs(trainingRef);
 
-      const fetchedTrainings: Training[] = [];
+			const fetchedTrainings: Training[] = [];
 
-      trainingDoc.forEach((doc) => {
-        const trainingData: Training = { ...(doc.data() as Training) };
-        fetchedTrainings.push(trainingData);
-      });
-      setTrainings(fetchedTrainings);
-    } catch (e) {
-      console.error(e);
-    }
-  };
+			trainingDoc.forEach((doc) => {
+				const trainingData: Training = { ...(doc.data() as Training) };
+				fetchedTrainings.push(trainingData);
+			});
+			setTrainings(fetchedTrainings);
+		} catch (e) {
+			console.error(e);
+		}
+	};
 
-  const [sortOption, setSortOption] = useState<string>("alphabetical"); // Default sorting option
-  const [selectedCategories, setSelectedCategories] = useState<string[]>([]); // Initialize
-  const [filteredTrainings, setFilteredTrainings] = useState<any[]>(trainings);
+	const [sortOption, setSortOption] = useState<string>('alphabetical'); // Default sorting option
+	const [selectedCategories, setSelectedCategories] = useState<string[]>([]); // Initialize
+	const [filteredTrainings, setFilteredTrainings] = useState<any[]>(trainings);
 
-  const handleSortChange = (newSortOption: string) => {
-    setSortOption(newSortOption);
-    sortTrainings(newSortOption);
-  };
+	const handleSortChange = (newSortOption: string) => {
+		setSortOption(newSortOption);
+		sortTrainings(newSortOption);
+	};
 
-  const sortTrainings = (option: string) => {
-    let sortedTrainings: any[] = [...filteredTrainings];
+	const sortTrainings = (option: string) => {
+		let sortedTrainings: any[] = [...filteredTrainings];
 
-    switch (option) {
-      case "alphabetical":
-        sortedTrainings.sort((a, b) =>
-          a.trainingName.localeCompare(b.trainingName)
-        );
-        break;
-      case "date":
-        sortedTrainings.sort((a, b) => b.trainingDate - a.trainingDate);
-        break;
-      case "registrants":
-        sortedTrainings.sort(
-          (a, b) =>
-            b.trainingRegistrants?.length - a.trainingRegistrants?.length
-        );
-        break;
-      default:
-        break;
-    }
+		switch (option) {
+			case 'alphabetical':
+				sortedTrainings.sort((a, b) => a.trainingName.localeCompare(b.trainingName));
+				break;
+			case 'date':
+				sortedTrainings.sort((a, b) => b.trainingDate - a.trainingDate);
+				break;
+			case 'registrants':
+				sortedTrainings.sort((a, b) => b.trainingRegistrants?.length - a.trainingRegistrants?.length);
+				break;
+			default:
+				break;
+		}
 
-    setFilteredTrainings(sortedTrainings);
-  };
+		setFilteredTrainings(sortedTrainings);
+	};
 
-  useEffect(() => {
+	useEffect(() => {
 		filterTrainings();
 	}, [selectedCategories]); // Run the effect whenever selectedCategories changes
 
-  useEffect(() => {
-    sortTrainings(sortOption);
-  }, [sortOption]);
+	useEffect(() => {
+		sortTrainings(sortOption);
+	}, [sortOption]);
 
-  const handleCategoryChange = (newSelectedCategories: string[]) => {
-    setSelectedCategories(newSelectedCategories);
-  };
+	const handleCategoryChange = (newSelectedCategories: string[]) => {
+		setSelectedCategories(newSelectedCategories);
+	};
 
-  const filterTrainings = () => {
-    if (selectedCategories.length > 0) {
-      setFilteredTrainings(
-        trainings.filter((training) =>
-          selectedCategories.includes(training.trainingCategory)
-        )
-      );
-    } else {
-      setFilteredTrainings(trainings);
-    }
-  };
+	const filterTrainings = () => {
+		if (selectedCategories.length > 0) {
+			setFilteredTrainings(trainings.filter((training) => selectedCategories.includes(training.trainingCategory)));
+		} else {
+			setFilteredTrainings(trainings);
+		}
+	};
 
-  return (
-    <main className="flex flex-col w-full p-4 md:p-6 lg:p-8 xl:p-10">
-      <section className="pb-8">
-        <h1 className="font-inter font-semibold pb-4 text-lg text-dark">
-          All Trainings
-        </h1>
-        <div>
-          <div className="pb-8">
-            <div className="flex gap-6 justify-between">
-              <div className="flex gap-6">
-                <CategoryDropdown
-                  selectedCategories={selectedCategories}
-                  onCategoryChange={handleCategoryChange}
-                />
-                <SortDropdown onSortChange={handleSortChange} />
-              </div>
-            </div>
-            <div className="flex flex-wrap gap-2 pt-4 ">
-              {selectedCategories
-                .slice()
-                .sort((a, b) => a.localeCompare(b))
-                .map((category, index) => (
-                  <CategoryBadge
-                    key={index}
-                    category={category}
-                  />
-                ))}
-            </div>
-          </div>
-          <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 w-full pb-8 bg-slate-50 p-6 rounded-xl'>
-            {filteredTrainings.map((training, index) => (
-              <TrainingCard
-                key={training.trainingId + "_" + index}
-                trainingData={training}
-              />
-            ))}
-            {trainings.length === 0 || filteredTrainings.length === 0 && (
-              <h1 className="justify-center font-semibold text-center col-span-full py-24">No trainings found</h1>
-            )}
-          </div>
-        </div>
-      </section>
-    </main>
-  );
+	return (
+		<main className='flex flex-col w-full p-4 md:p-6 lg:p-8 xl:p-10'>
+			<section className='pb-8'>
+				<h1 className='font-inter font-semibold pb-4 text-lg text-dark'>All Trainings</h1>
+				<div>
+					<div className='pb-8'>
+						<div className='flex gap-6 justify-between'>
+							<div className='flex gap-6'>
+								<CategoryDropdown selectedCategories={selectedCategories} onCategoryChange={handleCategoryChange} />
+								<SortDropdown onSortChange={handleSortChange} />
+							</div>
+						</div>
+						<div className='flex flex-wrap gap-2 pt-4 '>
+							{selectedCategories
+								.slice()
+								.sort((a, b) => a.localeCompare(b))
+								.map((category, index) => (
+									<CategoryBadge key={index} category={category} style={''} />
+								))}
+						</div>
+					</div>
+					<div className='grid grid-cols-1 lg:grid-cols-2 gap-6 w-full pb-8 bg-slate-50 p-6 rounded-xl'>
+						{filteredTrainings.map((training, index) => (
+							<TrainingCard key={training.trainingId + '_' + index} trainingData={training} />
+						))}
+						{trainings.length === 0 ||
+							(filteredTrainings.length === 0 && (
+								<h1 className='justify-center font-semibold text-center col-span-full py-24'>No trainings found</h1>
+							))}
+					</div>
+				</div>
+			</section>
+		</main>
+	);
 };
 
 export default AllTrainingsPage;
