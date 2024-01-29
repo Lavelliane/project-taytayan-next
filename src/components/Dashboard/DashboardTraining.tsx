6e996cd1c8a2f06adc8c7c60ef5b531bb461a7fe
Johnfil Initan <njohnfil.pr+github@gmail.com>
2024-01-29 14:02:19 +0800

    Changed number of columns for events/trainings grid for XL screens

--- src/components/Dashboard/DashboardTraining.tsx
+++ src/components/Dashboard/DashboardTraining.tsx
@@ -72,7 +72,7 @@ export const DashboardTraining = () => {
 						))}
 				</div>
 			</div>
-			<div className='grid grid-cols-1 lg:grid-cols-2 gap-6 w-full pb-8 bg-slate-50 p-6 rounded-xl'>
+			<div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 w-full pb-8 bg-slate-50 p-6 rounded-xl'>
 				{filteredTrainings.map((training: Training) => (
 					<TrainingCard key={training.trainingId} trainingData={training as Training} />
 				))}
--- src/components/Trainings/TrainingsPage.tsx
+++ src/components/Trainings/TrainingsPage.tsx
@@ -124,13 +124,14 @@ const TrainingsPage = () => {
 								))}
 						</div>
 					</div>
-					<div className='grid grid-cols-1 lg:grid-cols-2 gap-6 w-full pb-8 bg-slate-50 p-6 rounded-xl'>
+					<div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 w-full pb-8 bg-slate-50 p-6 rounded-xl'>
 						{filteredTrainings.map((training, index) => (
 							<MyTrainingCard key={training.trainingId + '_' + index} trainingData={training} />
 						))}
-						{trainings.length === 0 || filteredTrainings.length === 0 && (
-							<h1 className="justify-center font-semibold text-center col-span-full py-24">No trainings found</h1>
-						)}
+						{trainings.length === 0 ||
+							(filteredTrainings.length === 0 && (
+								<h1 className='justify-center font-semibold text-center col-span-full py-24'>No trainings found</h1>
+						))}
 					</div>
 				</div>
 			</section>
