661d8439a7cabe6b391f3b27369f44d7d288efbc
Vince Abella <vinceabella05@gmail.com>
2024-02-25 20:50:35 +0800

    fix error

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,12 +1,9 @@
-import FeedbackFormModal from '@/components/Feedback/FeedbackFormModal';
 import React from 'react';
 
 const page = () => {
 	return (
 		<div className='h-screen '>
-			<div>
-				<FeedbackFormModal addFeedbackOpen={true} />
-			</div>
+			<div></div>
 		</div>
 	);
 };
